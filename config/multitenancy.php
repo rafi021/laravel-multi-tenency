1204b9b9f50d5d9495ea706c4933cfaed007adeb
rafi021 <mahmud.ibrahim021@gmail.com>
2022-08-16 19:30:00 +0600

    CREATING TENANT DATABASES

--- config/multitenancy.php
+++ config/multitenancy.php
@@ -34,7 +34,10 @@ return [
      */
     'switch_tenant_tasks' => [
         // \Spatie\Multitenancy\Tasks\PrefixCacheTask::class,
+
         \Spatie\Multitenancy\Tasks\SwitchTenantDatabaseTask::class,
+
+
         // \Spatie\Multitenancy\Tasks\SwitchRouteCacheTask::class,
     ],
 
